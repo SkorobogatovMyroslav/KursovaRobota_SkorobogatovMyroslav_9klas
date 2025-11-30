268432a8dd232f03912418ced3dca4d6ebf17621
SkorobogatovMyroslav <skoromyr@gmail.com>
2025-11-30 23:56:56 +0200

    changeghung

--- JavaScript_курсова9клас.js
+++ JavaScript_курсова9клас.js
@@ -1,17 +1,15 @@
 document.getElementById("calculate").addEventListener("click", function () { 
     let totalPower = 0;
 
-    // Знаходимо всі блоки з приладами
     const devices = document.querySelectorAll(".device");
 
     devices.forEach(device => {
-        const power = Number(device.getAttribute("power")); // потужність приладу
-        const quantity = Number(device.querySelector("select").value); // кількість
+        const power = Number(device.getAttribute("power"));
+        const quantity = Number(device.querySelector("select").value);
 
         totalPower += power * quantity;
     });
 
-    // Виводимо результат
     document.getElementById("result").innerText =
         "Загальна потужність: " + totalPower + " Вт";
 
@@ -21,23 +19,21 @@ document.getElementById("calculate").addEventListener("click", function () {
 document.getElementById("recommend").addEventListener("click", function () {
     let power = window.calculatedPower || 0;
 
-    // Перехід на сторінку рекомендацій з передачею значення
     window.location.href = "HTML2сторінка_курсова9клас.html?power=" + power ;
 });
 
 if(result){
     result.items.forEach(html => {
-        container.innerHTML += html; // вставляємо готову карточку
+        container.innerHTML += html;
     });
 } else {
     container.innerHTML = "<p class='no-systems'>Підходящих систем не знайдено.</p>"
 };
 
-// ======= Ось новий блок для кнопок "Докладніше" =======
 document.querySelectorAll(".Knipka").forEach(button => {
     button.addEventListener("click", () => {
         const card = button.closest(".system-card");
         const info = card.querySelector(".full-info");
-        if(info) info.classList.toggle("hidden"); // показуємо/ховаємо блок
+        if(info) info.classList.toggle("hidden");
     });
 });
